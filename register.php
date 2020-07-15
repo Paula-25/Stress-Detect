1f39550192704cee4b722dee2cf9ab1e9690939a
Paula-25 <paulaulor@yahoo.com>
2020-07-16 00:11:36 +0100

    Implemented Full Design

--- register.php
+++ register.php
@@ -22,3 +22,22 @@ else {
         echo ("Registration succesful!");
 }
 ?>
+
+<div class="adjust contact">       
+            <h1>Stress-Detect</h1>
+         <h2>Capture your face and lets tell you how much rest you need</h2>
+        <p class="animate__animated animate__bounce"><b>COMING SOON!</b></p>
+    </div>
+
+    <div class="adjust contact-form">
+        <form id="contact-form" method="POST" action="register.php">
+            <input type="email" name="user_email" placeholder="Your Email" class="form-control" required>
+            <br><br>
+            <input type="submit" class="form-control submit" value="REGISTER">
+        </form>
+    </div>
+
+    <div class="showcase-top" >
+        <img src="assets/images/untitled-project (4).png"><hr>
+        <img src="assets/images/untitled-project@2x (14).png">
+    </div>
