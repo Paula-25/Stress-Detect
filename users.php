e03e1a1e4d2a855172eb1b78846177c019110be3
Paula-25 <paulaulor@yahoo.com>
2020-07-20 16:12:18 +0100

    checking server

--- users.php
+++ users.php
@@ -7,7 +7,7 @@ $email = $_POST['email'];
 //checking connection
 $connection= 
 mysqli_connect("x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "rd5q6ogkv51a0vqy", "raxbhv9xwycl28ep","ong2tttua61e8vuq"
-) or 
+) || 
     die('Error connecting to MySQL server');
 
 
@@ -15,7 +15,7 @@ if(isset($_POST)) {
     $email = $_POST['email'];
 
 //execute sql query
-$query = "INSERT INTO 'users' ('email') VALUES ('{$email}')";
+$query = "INSERT INTO 'users' ('email') VALUES ('$email')";
 $result=
 mysqli_query($connection, $query);
 
@@ -25,7 +25,7 @@ if (!$result)
     die ("Error in sql query");
 } 
 else {
-        echo "Registration succesful!";
+        echo "Registration succesful!" ;
     }
 
 }
